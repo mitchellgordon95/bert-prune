a51d4e719d39d83bbe058fdb2ce24c373c68cc61
Mitchell Gordon <mitchell.gordon95@gmail.com>
2019-04-28 12:10:14 -0400

    Add config options for UGE script

--- download_wiki_data.sh
+++ download_wiki_data.sh
@@ -1,5 +1,15 @@
+#! /bin/bash
+#$ -cwd
+#$ -V
+#$ -l num_proc=2,h_rt=10:00:00
+#$ -j y
+#$ -m ase
+#$ -M mitchell.gordon95@gmail.com
+
+# Above: configuration for running things on the univa grid engine
+
 mkdir data
 wget https://dumps.wikimedia.org/enwiki/latest/enwiki-latest-pages-articles.xml.bz2 -o data/enwiki-latest-pages-articles.xml.bz2
 bunzip2 data/enwiki-latest-pages-articles.xml.bz2
 git clone git@github.com:attardi/wikiextractor.git data/wikiextractor
-python data/wikiextractor/WikiExtractor.py -o data/enwiki data/enwiki-latest-pages-articles.xml
+python data/wikiextractor/WikiExtractor.py --json -o data/enwiki data/enwiki-latest-pages-articles.xml
