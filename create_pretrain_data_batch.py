5712400ad4eb22bef3385c3c3f0a1d5be611f6ba
Mitchell Gordon <mitchell.gordon95@gmail.com>
2019-05-04 13:30:26 -0400

    Fix the create pretrain batch script to always call with the right setting for max_predictions_per_seq_len

--- create_pretrain_data_batch.py
+++ create_pretrain_data_batch.py
@@ -2,7 +2,8 @@ from univa_grid import TaskRunner
 import subprocess
 import os
 
-MAX_SEQ_LEN = 512
+MAX_SEQ_LEN = 128
+MASKED_LM_PROB = 0.15
 try:
     os.mkdir(f'data/pretrain_examples_len_{MAX_SEQ_LEN}')
 except FileExistsError:
@@ -16,8 +17,8 @@ def create_pretrain(task_id):
         '--vocab_file', 'uncased_L-12_H-768_A-12/vocab.txt',
         '--do_lower_case', 'True',
         '--max_seq_length', str(MAX_SEQ_LEN),
-        '--max_predictions_per_seq', '20',
-        '--masked_lm_prob', '0.15',
+        '--max_predictions_per_seq', str(MAX_SEQ_LEN * MASKED_LM_PROB),
+        '--masked_lm_prob', str(MASKED_LM_PROB),
         '--random_seed', '12345',
         '--dupe_factor', '5'])
 
