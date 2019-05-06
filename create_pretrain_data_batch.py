808b5aa695126105ae1d03d2ca849209db1154e9
Mitchell Gordon <mitchell.gordon95@gmail.com>
2019-05-06 10:47:45 -0400

    Minor script changes

--- create_pretrain_data_batch.py
+++ create_pretrain_data_batch.py
@@ -2,8 +2,11 @@ from univa_grid import TaskRunner
 import subprocess
 import os
 
+import math
+
 MAX_SEQ_LEN = 128
 MASKED_LM_PROB = 0.15
+MAX_PREDICTIONS_PER_SEQ = math.ceil(MAX_SEQ_LEN * MASKED_LM_PROB)
 try:
     os.mkdir(f'data/pretrain_examples_len_{MAX_SEQ_LEN}')
 except FileExistsError:
@@ -17,7 +20,7 @@ def create_pretrain(task_id):
         '--vocab_file', 'uncased_L-12_H-768_A-12/vocab.txt',
         '--do_lower_case', 'True',
         '--max_seq_length', str(MAX_SEQ_LEN),
-        '--max_predictions_per_seq', str(MAX_SEQ_LEN * MASKED_LM_PROB),
+        '--max_predictions_per_seq', str(MAX_PREDICTIONS_PER_SEQ),
         '--masked_lm_prob', str(MASKED_LM_PROB),
         '--random_seed', '12345',
         '--dupe_factor', '5'])
--- run_pretrain_grid.sh
+++ run_pretrain_grid.sh
@@ -5,20 +5,22 @@
 #$ -j y
 #$ -m ase
 #$ -M mitchell.gordon95@gmail.com
-#$ -q gpu.q@@1080
+#$ -q gpu.q@@titanxp
+
+# Note: Titan XP gpus have the most memory on the grid (12 GB)
 
 ml load cuda90/toolkit
 
 BERT_BASE_DIR="uncased_L-12_H-768_A-12_prunable"
 python run_pretraining.py \
-  --input_file=data/pretrain_examples_len_512/tf_examples*.tfrecord \
-  --output_dir=data/pretraining_output \
+  --input_file=data/pretrain_examples_len_128/tf_examples*.tfrecord \
+  --output_dir=models/pretraining_output \
   --do_train=True \
   --do_eval=True \
   --bert_config_file=$BERT_BASE_DIR/bert_config.json \
   --init_checkpoint=$BERT_BASE_DIR/bert_model.ckpt \
   --train_batch_size=32 \
-  --max_seq_length=512 \
+  --max_seq_length=128 \
   --max_predictions_per_seq=20 \
   --num_train_steps=20 \
   --num_warmup_steps=10 \
