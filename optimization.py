7add6e8b09e15aee71f34a9387fa470eaf5dd7fc
Mitchell Gordon <mitchell.gordon95@gmail.com>
2019-06-28 10:46:04 -0400

    Add gradient checkpointing so we can train with basically any GPU (and bigger batches)

--- optimization.py
+++ optimization.py
@@ -20,6 +20,7 @@ from __future__ import print_function
 
 import re
 import tensorflow as tf
+import memory_saving_gradients
 
 # Pruning stuff
 from tensorflow.contrib.model_pruning.python.pruning import Pruning, get_pruning_hparams
@@ -71,7 +72,7 @@ def create_optimizer(loss, init_lr, num_train_steps, num_warmup_steps, use_tpu, 
     optimizer = tf.contrib.tpu.CrossShardOptimizer(optimizer)
 
   tvars = tf.trainable_variables()
-  grads = tf.gradients(loss, tvars)
+  grads = memory_saving_gradients.gradients(loss, tvars, checkpoints='memory')
 
   # This is how the model was pre-trained.
   (grads, _) = tf.clip_by_global_norm(grads, clip_norm=1.0)
--- run_uge_gpu.sh
+++ run_uge_gpu.sh
@@ -1,13 +1,14 @@
 #! /bin/bash
 #$ -cwd
 #$ -V
-#$ -l gpu=1,h_rt=48:00:00,num_proc=2,mem_free=3G
+#$ -l gpu=1,h_rt=96:00:00,num_proc=2,mem_free=3G
 #$ -j y
 #$ -m ase
 #$ -M mitchell.gordon95@gmail.com
-#$ -q gpu.q@@titanxp
+#$ -q gpu.q
 
 # Note: Titan XP gpus have the most memory on the grid (12 GB)
+# However, with gradient checkpointing we can get away with using basically any GPU
 
 ml load cuda90/toolkit
 export PYTHONPATH=.
