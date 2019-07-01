d50219627f542fe0806e97476c31d298331bf1a1
Mitchell Gordon <mitchell.gordon95@gmail.com>
2019-07-01 16:06:05 -0400

    Update the UGE script to only use the 2080 gpu's (and print some debug info)

--- run_uge_gpu.sh
+++ run_uge_gpu.sh
@@ -1,15 +1,18 @@
 #! /bin/bash
 #$ -cwd
 #$ -V
-#$ -l gpu=1,h_rt=96:00:00,num_proc=2,mem_free=3G
+#$ -l gpu=1,h_rt=72:00:00,num_proc=2,mem_free=3G
 #$ -j y
 #$ -m ase
 #$ -M mitchell.gordon95@gmail.com
-#$ -q gpu.q
+#$ -q gpu.q@@2080
 
 # Note: Titan XP gpus have the most memory on the grid (12 GB)
 # However, with gradient checkpointing we can get away with using basically any GPU
 
-ml load cuda90/toolkit
+ml load cuda10.0/toolkit
+echo "CUDA_VISIBLE_DEVICES: ${CUDA_VISIBLE_DEVICES}"
+nvidia-smi
+conda list
 export PYTHONPATH=.
 python $1 "${@:2}"
