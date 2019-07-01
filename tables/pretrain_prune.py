c38a7f553ec09778c9ff81be7d7d13b2d4f8a002
Mitchell Gordon <mitchell.gordon95@gmail.com>
2019-07-01 16:04:51 -0400

    Update the GLUE results table generator with pretrain loss

--- tables/pretrain_prune.py
+++ tables/pretrain_prune.py
@@ -5,34 +5,52 @@ eval_loss = {eval_loss:f}
 global_step = {step:d}
 loss = {loss:f}"""
 
+PRETRAIN_EVAL_RESULTS_TEMPLATE = """global_step = {global_step:d}
+loss = {loss:f}
+masked_lm_accuracy = {masked_lm_accuracy:f}
+masked_lm_loss = {masked_lm_loss:f}
+next_sentence_accuracy = {next_sentence_accuracy:f}
+next_sentence_loss = {next_sentence_loss:f}
+"""
+
+def parse_file(fname, template):
+    try:
+        with open(fname, 'r') as f:
+            return parse(template, f.read())
+    except FileNotFoundError:
+        print(f"Missing {fname}")
+        return None
+
 table_rows = []
 for sparsity in [0, .4, .5, .6, .7, .8, .9]:
-    task_accuracies = []
+    row_entries = []
+
+    pretrain_results = parse_file(f'models/pretrain/gradual_prune_{int(sparsity*100)}/eval_results.txt', PRETRAIN_EVAL_RESULTS_TEMPLATE)
+    if pretrain_results:
+        row_entries.append(pretrain_results['loss'])
+    else:
+        row_entries.append(0)
 
     # Find the best accuracy for each task
-    for task in ['MNLI', 'QQP', 'QNLI', 'SST-2', 'CoLA', 'MRPC', 'XNLI', 'RTE']:
+    for task in ['MNLI', 'QQP', 'QNLI', 'SST-2', 'CoLA', 'STS-B', 'MRPC', 'RTE']:
 
         # Select the model with the best accuracy among the grid search
         grid_search_acc = []
         for lr in ['2e-5','3e-5','4e-5','5e-5']:
-            fname = f'models/{task}/gradual_prune_{int(sparsity*100)}_lr_{lr}/eval_results.txt'
-            try:
-                with open(fname, 'r') as f:
-                    acc = parse(EVAL_RESULTS_TEMPLATE, f.read())['eval_accuracy']
-                    grid_search_acc.append(acc)
-            except FileNotFoundError:
-                print(f"Missing {fname}")
+            downstream_results = parse_file(f'models/{task}/gradual_prune_{int(sparsity*100)}_lr_{lr}/eval_results.txt', EVAL_RESULTS_TEMPLATE)
+            if downstream_results:
+                grid_search_acc.append(downstream_results['eval_accuracy'])
 
-        task_accuracies.append(max(grid_search_acc, default=0))
+        row_entries.append(max(grid_search_acc, default=0))
 
     table_rows.append(
-        f"{sparsity} & " + " & ".join([f"{acc:.2f}" for acc in task_accuracies])
+        f"{sparsity} & " + " & ".join([f"{acc:.2f}" for acc in row_entries])
     )
 
 rows = "\\\\\n".join(table_rows)
 print(f"""
-\\begin{{tabular}}{{|c||c|c|c|c|c|c|c|c|}}
-\\hline Pruned & MNLI & QQP & QNLI & SST-2 & CoLA & STS-B & MRPC & RTE \\\\
+\\begin{{tabular}}{{|c|c||c|c|c|c|c|c|c|c|}}
+\\hline Pruned & Pre-train Loss & MNLI & QQP & QNLI & SST-2 & CoLA & STS-B & MRPC & RTE \\\\
 \\hline
 {rows}\\\\
 \\end{{tabular}}
