97faad4decc5063aa11c65459e2f60ffcd3427c4
Mitchell Gordon <mitchell.gordon95@gmail.com>
2019-07-13 12:01:43 -0400

    Refactor downstream experiments to allow model naming flexibility

--- downstream_experiments/common.py
+++ downstream_experiments/common.py
@@ -7,7 +7,8 @@ GLUE_DIR = "data/glue_data"
 
 def _run_classifier(
         task_name,
-        init_model_name,
+        init_model_dir,
+        model_name,
         do_train,
         do_eval,
         do_predict,
@@ -17,25 +18,25 @@ def _run_classifier(
     train_args = ["--do_train=True", "--num_train_epochs", str(num_train_epochs)] if do_train else []
     eval_args = ["--do_eval=True"] if do_eval else []
     predict_args = ["--do_predict=True"] if do_predict else []
+    init_args = ["--init_checkpoint", init_model_dir] if init_model_dir else []
     subprocess.call([
         "python", "run_classifier.py",
         "--task_name", task_name,
         "--data_dir", f'{GLUE_DIR}/{task_name}',
-        "--output_dir", os.path.join(MODELS_DIR, task_name, f'{init_model_name}_lr_{lr}'),
+        "--output_dir", os.path.join(MODELS_DIR, task_name, f'{model_name}_lr_{lr}'),
         "--bert_config_file", f"{BERT_BASE_DIR}/bert_config.json",
-        "--init_checkpoint", f"{MODELS_DIR}/pretrain/{init_model_name}",
         "--vocab_file", f"{BERT_BASE_DIR}/vocab.txt",
         "--train_batch_size", "32",
         "--max_seq_length", "128",
         "--keep_checkpoint_max", "2",
-        "--learning_rate", "2e-5"] + train_args + eval_args + predict_args
+        "--learning_rate", lr] + train_args + eval_args + predict_args + init_args
     )
 
-def train(task_name, init_model_name, num_train_epochs, lr):
-    _run_classifier(task_name, init_model_name, do_train=True, do_eval=False, do_predict=False, num_train_epochs=num_train_epochs, lr=lr)
+def train(task_name, init_model_dir, model_name, num_train_epochs, lr):
+    _run_classifier(task_name, init_model_dir, model_name, do_train=True, do_eval=False, do_predict=False, num_train_epochs=num_train_epochs, lr=lr)
 
-def eval_(task_name, init_model_name, lr):
-    _run_classifier(task_name, init_model_name, do_train=False, do_eval=True, do_predict=False, lr=lr)
+def eval_(task_name, model_name, lr):
+    _run_classifier(task_name, None, model_name, do_train=False, do_eval=True, do_predict=False, lr=lr)
 
-def predict(task_name, init_model_name):
-    _run_classifier(task_name, init_model_name, do_train=False, do_eval=False, do_predict=True)
+def predict(task_name, model_name):
+    _run_classifier(task_name, None, model_name, do_train=False, do_eval=False, do_predict=True)
--- downstream_experiments/glue_sign_ticket.py
+++ downstream_experiments/glue_sign_ticket.py
@@ -1,14 +1,15 @@
-import subprocess
 from univa_grid import TaskRunner
-from downstream_experiments.common import train, eval_, predict
+from downstream_experiments.common import train, eval_, predict, MODELS_DIR
 
 def train_downstream(sparsity):
-    init_model_name = f"gradual_prune_{int(sparsity*100)}_sign_ticket"
-    for task in ['MNLI', 'QQP', 'QNLI', 'SST-2', 'CoLA', 'STS-B', 'MRPC', 'RTE']:
-        for lr in ['2e-5','3e-5','4e-5','5e-5',]:
+    model_name = f"gradual_prune_{int(sparsity*100)}_sign_ticket"
+    init_model_dir = f"{MODELS_DIR}/pretrain/{model_name}"
+    # This is the order from least training data to most training data
+    for task in [ 'RTE',  'MRPC', 'STS-B',  'CoLA', 'SST-2', 'QNLI', 'QQP', 'MNLI']:
+        for lr in ['2e-5','3e-5','4e-5','5e-5']:
             for epoch in range(4):
-                train(task, init_model_name, epoch, lr)
-                eval_(task, init_model_name, lr)
+                train(task, init_model_dir, model_name, epoch, lr)
+                eval_(task, model_name, lr)
 
 task_runner = TaskRunner()
 for sparsity in [0, .4, .5, .6, .7, .8, .9]: # -t 1-7 for univa grid engine
