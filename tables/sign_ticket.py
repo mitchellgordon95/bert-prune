20d129bcfda1612a5ff539fcc1be732247e9647c
Mitchell Gordon <mitchell.gordon95@gmail.com>
2019-07-16 15:20:17 -0400

    Add table for sign tickets

--- tables/sign_ticket.py
+++ tables/sign_ticket.py
@@ -25,33 +25,26 @@ table_rows = []
 for sparsity in [0, .4, .5, .6, .7, .8, .9]:
     row_entries = []
 
-    pretrain_results = parse_file(f'models/pretrain/gradual_prune_{int(sparsity*100)}/eval_results.txt', PRETRAIN_EVAL_RESULTS_TEMPLATE)
-    if pretrain_results:
-        row_entries.append(pretrain_results['loss'])
-    else:
-        row_entries.append(0)
-
     # Find the best accuracy for each task
-    for task in ['MNLI', 'QQP', 'QNLI', 'SST-2', 'CoLA', 'STS-B', 'MRPC', 'RTE']:
+    for task in ['MNLI', 'QQP', 'QNLI', 'SST-2', 'CoLA', 'MRPC', 'RTE']: # 'STS-B'
 
         # Select the model with the best accuracy among the grid search
         grid_search_acc = []
         for lr in ['2e-5','3e-5','4e-5','5e-5']:
-            downstream_results = parse_file(f'models/{task}/gradual_prune_{int(sparsity*100)}_lr_{lr}/eval_results.txt', EVAL_RESULTS_TEMPLATE)
+            downstream_results = parse_file(f'models/{task}/gradual_prune_{int(sparsity*100)}_sign_ticket_lr_{lr}/eval_results.txt', EVAL_RESULTS_TEMPLATE)
             if downstream_results:
                 grid_search_acc.append(downstream_results['eval_accuracy'])
 
         row_entries.append(max(grid_search_acc, default=0))
 
     table_rows.append(
-        f"{sparsity} & " + " & ".join([f"{acc:.2f}" for acc in row_entries])
+        f"{sparsity} & ? & " + " & ".join([f"{acc:.2f}" for acc in row_entries])
     )
 
 rows = "\\\\\n".join(table_rows)
 print(f"""
-\\begin{{tabular}}{{cccccccccc}}
-Pruned & Pre-train Loss & MNLI & QQP & QNLI & SST-2 & CoLA & STS-B & MRPC & RTE \\\\
-\hline
+  &  & \\multicolumn{{8}}{{c}}{{Reinitialized}} \\\\
+\\hline
 {rows}\\\\
 \\end{{tabular}}
 """)
